17f13ca6c98758135ee5ac4e96dfee43bb866a69
Grant Sherman <tnarg122@gmail.com>
2021-03-27 12:45:25 -0400

    Reworked solution using the STL.

--- 2020/day_1.cpp
+++ 2020/day_1.cpp
@@ -1,19 +1,20 @@
 #include <algorithm>
-#include <array>
+#include <array>  // remove me
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
-/** Function to read the contents of a text file and add them to a list of
- * integers.
+/** Function to read the contents of a text file and add them to a vector.
  * @param file_name is the name of the file to read.
- * @returns a list<int> containing all of the data from file_name.
+ * @returns a vector<T> containing all of the data from file_name.
  */
-std::array<int, 200> ParseInput(std::string file_name) {
+template <class T>
+std::vector<T> ParseInput(std::string file_name) {
   // Open file.
   std::ifstream file(file_name);
-  // List to hold contents of file.
-  std::array<int, 200> l;
+  // Vector to hold contents of file.
+  std::vector<T> v;
   // String to temporarily hold contents of line being read in file.
   std::string line;
 
@@ -22,83 +23,89 @@ std::array<int, 200> ParseInput(std::string file_name) {
     int i = 0;
     // Read in all of the lines in the file.
     while (getline(file, line)) {
-      // Append the contents to the list.
-      l[i] = std::stoi(line);
+      // Append the contents to the vector.
+      // TODO: Make this more generic.
+      v.push_back(std::stoi(line));
       ++i;
     }
   } catch (const std::exception &e) {
     std::cerr << e.what() << '\n';
   }
 
-  // Return the populated list.
-  return l;
+  // Return the populated vector.
+  return v;
 }
 
-int main() {
-  // List to hold puzzle input.
-  std::array<int, 200> input = ParseInput("day_1_input.txt");
-
-  /** Part 1 Puzzle Algorithm
-   * Can do a little better than brute force searching through the entire list.
-   * 1. First, sort the list into ascending order.
-   * 2. Start with the first number in the list as a and the last number b.
-   * 3. Add a and b together to get the sum c.
-   * 4. If c is equal to 2020, then multiply a and b together to solve the
-   * puzzle.
-   * 5. If c is greater than 2020, then replace b with the number previous to it
-   * in the list (the next smallest).
-   * 6. If c is less than 2020, then replace a with the number after it in the
-   * list (the next largest) and reset b to the last number in the list.
-   * 7. Repeat steps 3-6 until the condition in step 4 is met to solve the
-   * puzzle.
-   */
-
-  // Sort the list into ascending order.
-  std::sort(input.begin(), input.end());
-
-  // Print out list contents to double-check.
-  for (int n : input) {
+/** Function to print out a container.
+ * @param container is a STL container to print the contents of.
+ */
+template <class T>
+void PrintContainer(T container) {
+  for (int n : container) {
     std::cout << n << ", ";
   }
 
   std::cout << std::endl;
+}
 
-  // First array index.
-  int first = 0;
-
-  // Last array index.
-  int last = input.size();
-
-  // Set a equal to the first element.
-  int a = input[first];
-
-  // Set b equal to the last element.
-  int b = input[last];
-
-  // Sum of a and b.
-  int c = a + b;
-
-  // Target sum to end search at.
-  int target = 2020;
-
-  while (c != target) {
-    if (c > target) {
-      --last;
-      b = input[last];
-    } else {
-      ++first;
-      a = input[first];
-      last = input.size();
-      b = input[last];
-    }
-    c = a + b;
+/** Part 1 Puzzle Algorithm
+ * Can do a little better than brute force searching through the entire
+ * container.
+ * 1. First, sort the container into ascending order.
+ * 2. Start with a pointer 'a' pointing to the first member of the container and
+ * pointer 'b' pointing to the last member.
+ * 3. If a and b are pointing to the same number then they've overlapped meaning
+ * there's no solution or there is an error.
+ * 4. Add the values a and b are pointing at together to get the sum c.
+ * 5. If c is equal to 2020, then multiply the values a and b are pointing at
+ * together to solve the puzzle.
+ * 6. If c is greater than 2020, then decrement b.
+ * 7. If c is less than 2020, then increment a.
+ * 8. Repeat steps 3-7 until the condition 3 or 5 is met.
+ * @param x is the target sum of the 2 values.
+ * @param v is the vector which holds the numbers to search through.
+ * @param a is the iterator pointer at the beginning of the vector v.
+ * @param b is the iterator pointer at the end of vector v.
+ * @returns -1 if no solution is found, otherwise the product of a * b whose sum
+ * is x.
+ */
+int SolutionPartOne(int x, std::vector<int> v, std::vector<int>::iterator a,
+                    std::vector<int>::iterator b) {
+  // If pointers are pointing to the same address then there is either no
+  // solution or an error has occured.
+  if (a == b) {
+    return -1;
   }
 
-  int solution = a * b;
+  // Calculate the product of a and b.
+  int c = *a + *b;
+  // std::cout << *a << " " << * b << " " << c << std::endl;
+
+  if (c == x) {
+    // C matches x so we can return the product of a and b.
+    return *a * *b;
+  } else if (c > x) {
+    // C is greater than x so decrement b and run again.
+    return SolutionPartOne(x, v, a, --b);
+  } else {
+    // C is less than x so increment a and run again.
+    return SolutionPartOne(x, v, ++a, b);
+  }
+}
+int main() {
+  // List to hold puzzle input.
+  std::vector<int> input = ParseInput<int>("day_1_input.txt");
+
+  // Sort the vector.
+  std::sort(input.begin(), input.end());
+
+  // Print out vector contents to double-check.
+  PrintContainer<std::vector<int>>(input);
+
+  // Calculate the solution using the algorithm described in the method above.
+  int solution = SolutionPartOne(2020, input, input.begin(), --input.end());
 
   std::cout << "Part 1\n"
-            << "a = " << a << "\n"
-            << "b = " << b << "\n"
             << "The answer is: " << solution << std::endl;
 
   /** Part 2 Puzzle Algorithm
@@ -120,7 +127,7 @@ int main() {
    * 9. Repeat steps 3-8 until the condition in step 4 is met to solve the
    * puzzle.
    */
-
+  /*
   int second = first + 1;
   last = input.size();
 
@@ -157,13 +164,16 @@ int main() {
             << "b = " << b << "\n"
             << "c = " << c << "\n"
             << "The answer is: " << solution << std::endl;
+  */
 }
 
 /** What I learned with this exercise:
  *
- * I definitely should have made each part of the solution its own function so
- * that I didn't run into the issue of having to reuse or rename variable to
- * solve part 2. Knowing how the puzzles work with two parts will come in handy
- * in the future.
+ * 3/10/21 - I definitely should have made each part of the solution its own
+ * function so that I didn't run into the issue of having to reuse or rename
+ * variable to solve part 2. Knowing how the puzzles work with two parts will
+ * come in handy in the future.
+ *
+ * 3/27/21 - I updated the solution to use templates and the STL.
  *
  */
