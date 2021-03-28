ccd896deea822c81c31444beccd2d8dc5a27e8ac
Grant Sherman <tnarg122@gmail.com>
2021-03-28 01:53:03 -0400

    Day 1 part 2 solution implemented. Day 1 complete.

--- 2020/day_1.cpp
+++ 2020/day_1.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <array>  // remove me
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -7,14 +6,13 @@
 
 /** Function to read the contents of a text file and add them to a vector.
  * @param file_name is the name of the file to read.
- * @returns a vector<T> containing all of the data from file_name.
+ * @returns a vector<int> containing all of the data from file_name.
  */
-template <class T>
-std::vector<T> ParseInput(std::string file_name) {
+std::vector<int> ParseInput(std::string file_name) {
   // Open file.
   std::ifstream file(file_name);
   // Vector to hold contents of file.
-  std::vector<T> v;
+  std::vector<int> v;
   // String to temporarily hold contents of line being read in file.
   std::string line;
 
@@ -92,9 +90,61 @@ int SolutionPartOne(int x, std::vector<int> v, std::vector<int>::iterator a,
     return SolutionPartOne(x, v, ++a, b);
   }
 }
+
+/** Part 2 Puzzle Algorithm
+ * Can do a little better than brute force searching through the entire
+ * container.
+ * 1. First, sort the container into ascending order.
+ * 2. Start with a pointer 'a' pointing to the first member of the container and
+ * pointer 'b' pointing to the member after a and pointer 'c' pointing to the
+ * last member.
+ * 3. If a and c are pointing to the same number then they've overlapped meaning
+ * there's no solution or there is an error.
+ * 4. If b and c are pointing to the same number then they've overlapped meaning
+ * the solution is not between a and c so increment a and reset b to a + 1.
+ * 4. Add the values a, b and c are pointing at together to get the sum i.
+ * 5. If i is equal to 2020, then multiply the values a, b, and c are pointing
+ * at together to solve the puzzle.
+ * 6. If i is greater than 2020 and a and b are the smallest two numbers in the
+ * vector, then decrement c.
+ * 7. Otherwise increment b.
+ * 8. Repeat steps 4-7 until the condition 3 or 5 is met.
+ * @param x is the target sum of the 2 values.
+ * @param v is the vector which holds the numbers to search through.
+ * @param a is the iterator pointer at the beginning of the vector v.
+ * @param b is the iterator pointer at the end of vector v.
+ * @returns -1 if no solution is found, otherwise the product of a * b whose sum
+ * is x.
+ */
+int SolutionPartTwo(int x, std::vector<int> v, std::vector<int>::iterator a,
+                    std::vector<int>::iterator b,
+                    std::vector<int>::iterator c) {
+  // If pointers are pointing to the same address then there is either no
+  // solution or an error has occured.
+  if (a == c) {
+    return -1;
+  }
+
+  if (b == c) {
+    return SolutionPartTwo(x, v, ++a, a + 1, c);
+  }
+
+  // Calculate the product of a, b, and c.
+  int i = *a + *b + *c;
+  std::cout << *a << " " << *b << " " << *c << std::endl;
+
+  if (i == x) {
+    return *a * *b * *c;
+  } else if (i > x && a == b - 1) {
+    return SolutionPartTwo(x, v, a, a + 1, --c);
+  } else {
+    return SolutionPartTwo(x, v, a, ++b, c);
+  }
+}
+
 int main() {
   // List to hold puzzle input.
-  std::vector<int> input = ParseInput<int>("day_1_input.txt");
+  std::vector<int> input = ParseInput("day_1_input.txt");
 
   // Sort the vector.
   std::sort(input.begin(), input.end());
@@ -102,69 +152,20 @@ int main() {
   // Print out vector contents to double-check.
   PrintContainer<std::vector<int>>(input);
 
-  // Calculate the solution using the algorithm described in the method above.
+  // Calculate the solution to part 1 using the algorithm described in the
+  // method above.
   int solution = SolutionPartOne(2020, input, input.begin(), --input.end());
 
   std::cout << "Part 1\n"
             << "The answer is: " << solution << std::endl;
 
-  /** Part 2 Puzzle Algorithm
-   * Using the sorted list we can do a little better than brute force again.
-   * 1. First, sort the list into ascending order.
-   * 2. Start with a as the first in the list, b as the second, and c as the
-   * last.
-   * 3. Add a, b, and c together to get the sum d.
-   * 4. If d is equal to 2020, then multiply a, b, and c together to solve the
-   * puzzle.
-   * 5. If d is greater than 2020, then replace c with the number previous to it
-   * in the list (the next smallest).
-   * 6. If d is less than 2020, then replace b with the number after it in the
-   * list (the next largest).
-   * 7. Repeat steps 5 and 6 until a solution is reached or b equals c.
-   * 8. If a solution isn't found, replace a with b and b with the number after
-   * it in the list. Reset c with the last number in the list then repeat
-   * steps 6-8.
-   * 9. Repeat steps 3-8 until the condition in step 4 is met to solve the
-   * puzzle.
-   */
-  /*
-  int second = first + 1;
-  last = input.size();
-
-  a = input[first];
-  b = input[second];
-  c = input[last];
-  int d = a + b + c;
-  // Target is still 2020.
-
-  while (d != target) {
-    if (d > 2020) {
-      --last;
-      c = input[last];
-    } else {
-      ++second;
-      b = input[second];
-    }
-
-    if (b == c) {
-      second = ++first;
-      a = b;
-      b = input[second];
-      last = input.size();
-      c = input[last];
-    }
-    d = a + b + c;
-    std::cout << d << std::endl;
-  }
-
-  solution = a * b * c;
+  // Calculate the solution to part 2 using the algorithm described in the
+  // method above.
+  solution = SolutionPartTwo(2020, input, input.begin(), input.begin() + 1,
+                             --input.end());
 
   std::cout << "Part 2\n"
-            << "a = " << a << "\n"
-            << "b = " << b << "\n"
-            << "c = " << c << "\n"
             << "The answer is: " << solution << std::endl;
-  */
 }
 
 /** What I learned with this exercise:
